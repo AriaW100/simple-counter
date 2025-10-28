b4f681517f58fa90cb026fa6d951f7a184093e9f
Arya <166193266+AriaW100@users.noreply.github.com>
2025-10-28 09:36:56 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+
+document.getElementById("increaseBtn").onclick = function () {
+    count ++;
+    document.getElementById("countLabel").textContent = count;
+
+}
+
+document.getElementById("decreaseBtn").onclick = function () {
+    count --;
+    document.getElementById("countLabel").textContent = count;
+
+}
+
+document.getElementById("resetBtn").onclick = function () {
+    count = 0;
+    document.getElementById("countLabel").textContent = count;
+
+}
+
